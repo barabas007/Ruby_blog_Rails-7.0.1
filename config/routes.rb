79593a5cdc878e786652b277095fc7e3f7577608
barabas007 <shop.ru.shop@mail.ru>
2022-02-15 18:44:24 +0300

    added form title and body

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  resources :questions, only: %i[index new edit create]
   get '/questions', to: 'questions#index'
+
+  get '/questions/new', to: 'questions#new'
+
+  get '/questions/:id/edit', to: 'questions#edit'
+
+  post '/questions', to: 'questions#create'
+
   root 'pages#index'
 end
